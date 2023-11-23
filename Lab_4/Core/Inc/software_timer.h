8d1dec7f9ae70d488b1f2f338a6b49ee163e1a13
VAIO\Minhq <minhquan482212@gmail.com>
2023-11-23 23:36:09 +0700

    First Example for Cooperative Scheduler

---
+++ Lab_4/Core/Inc/software_timer.h
@@ -0,0 +1,30 @@
+/*
+ * software_timer.h
+ *
+ *  Created on: Oct 12, 2023
+ *      Author: Minhq
+ */
+
+#ifndef INC_SOFTWARE_TIMER_H_
+#define INC_SOFTWARE_TIMER_H_
+
+extern int timer1_flag;
+extern int timer2_flag;
+extern int timer3_flag;
+extern int timer4_flag;
+extern int timer5_flag;
+extern int TIMER_CYCLE;
+
+extern int timer0_mode_debounce_flag;
+extern int timer0_mode_debounce_counter;
+
+void set_Timer_mode_debounce(int duration);
+void setTimer1(int duration);
+void setTimer2(int duration);
+void setTimer3(int duration);
+void setTimer4(int duration);
+void setTimer5(int duration);
+void timerRun();
+void clearTimer1();
+
+#endif /* INC_SOFTWARE_TIMER_H_ */
